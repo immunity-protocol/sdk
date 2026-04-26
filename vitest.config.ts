d7b7e4210e78d12cf4454614258c8c237c26203d
dtucker <davidt2003@msn.com>
2026-04-25 23:52:14 -0400

    chore: vitest unit and integration configs

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["test/unit/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json-summary", "html", "lcov"],
+      include: ["src/**/*.ts"],
+      exclude: ["src/index.ts"],
+    },
+  },
+});
